7c97ce8b8a55622d40065d0eb408ac1ae34e5d51
FazliddinMirzaqosimov <fazliddinmirzaqosimov8@gmail.com>
2023-03-26 07:35:45 +0500

    cors addded

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const orderRoutes = require("./routers/orderRouter");
 const userRoutes = require("./routers/userRouter");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/users", userRoutes);
